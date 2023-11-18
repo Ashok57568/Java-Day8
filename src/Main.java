c4769db8a3b7cbe8de6a7b3b5774dca8bc12b33d
Ashok Dhungana <you@example.com>
2023-11-18 19:31:24 +0545

    Java Day

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,37 @@
-import java.util.*;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
-        Scanner sc=new Scanner(System.in);
+public class Main{
 
-        System.out.println("Menu");
-        System.out.println("====");
-        System.out.println("ADD");
-        System.out.println("SUB");
-        System.out.println("MUL");
-        System.out.println("DIV");
+    public static void main(String[] args) {
 
-        System.out.println("Enter 2 Numbers");
-        int x=sc.nextInt();
-        int y=sc.nextInt();
-        sc.nextLine();
-        System.out.println("Enter Option in Words ");
-        String option=sc.nextLine();
-        option=option.toUpperCase();
-
-        switch(option)
+        int i=1;//100
+        while(i<100)
         {
-            case "ADD": System.out.println("Sum is "+(x+y));
-                break;
-            case "SUB": System.out.println("Difference is "+(x-y));
-                break;
-            case "MUL": System.out.println("Product is "+(x*y));
-                break;
-            case "DIV": System.out.println("Ratio is "+(x/y));
-                break;
-            default:     System.out.println("Invalid Option");
-                break;
+            System.out.println(i);
+            i=i*2;
         }
 
+
+        {
+            System.out.println(i);
+            i=i*2;
+        }while(i<100);
+
+        byte i=1;
+        while(true)
+        {
+            System.out.println(i);
+            i++;
+        }
+
+        byte i=1;
+
+        if(true)
+        {
+            System.out.println(i);
+            i++;
+        }
+        else
+            System.out.println("Bye");
     }
+
 }
